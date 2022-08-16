9213930c6351f0089bd8b713fce9d397f7e90fd7
gnawre31 <erwangli2@gmail.com>
2022-08-15 21:19:48 -0400

    tracks

--- client/src/components/track/Track.jsx
+++ client/src/components/track/Track.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { useSpotify } from '../../context/spotify/SpotifyState'
 
 const Track = (props) => {
-    const {track} = props
+  const { track } = props
+  const [spotifyState] = useSpotify()
+
+  const currTrackStyling = spotifyState.playerState && spotifyState.playerState.track && spotifyState.playerState.track.id === track.id ? " bg-gray-600" : "bg-gray-900"
   return (
-    <div>
-        <div>{track.name}</div>
+    <div className={`h-20 mb-2 rounded-lg + ${currTrackStyling}`} >
+      <div className='flex'>
+        <img className="h-20 mr-2 w-20 rounded-tl-sm rounded-bl-lg" src={track.album.images[2].url} />
+        <div className='w-4/6 pr-2 flex flex-col justify-center'>
+          <div className='text-overflow truncate font-bold text-lg pr-4'>{track.name}</div>
+          <div className='flex truncate text-overflow '>{track.artists.map((a, idx) =>
+            <div key={idx} className="" >{a.name.toString() + (idx !== track.artists.length - 1 ? "," : "")}&nbsp;</div>
+
+
+          )}</div>
+        </div>
+
+      </div>
+
+
+      {/* <div>{JSON.stringify(track.album.images[0])}</div> */}
     </div>
   )
 }
--- client/src/components/track/TrackList.jsx
+++ client/src/components/track/TrackList.jsx
@@ -3,14 +3,14 @@ import { useSpotify } from '../../context/spotify/SpotifyState'
 import Track from './Track'
 
 const TrackList = () => {
-    const [spotifyState] = useSpotify()
-    const { tracks } = spotifyState
+  const [spotifyState] = useSpotify()
+  const { tracks } = spotifyState
   return (
-    <div className="overflow-y-auto w-1/4">
-        <div>Tracks</div>
-        <div>
-            {tracks.map((track, idx) => <Track key={idx} track={track}/>)}
-        </div>
+    <div className="overflow-y-auto w-1/4 pt-12 pb-12 pr-4 pl-2">
+      <div className='text-2xl font-bold mb-4'>Tracks</div>
+      <div>
+        {tracks.map((track, idx) => <Track key={idx} track={track} />)}
+      </div>
 
     </div>
   )
