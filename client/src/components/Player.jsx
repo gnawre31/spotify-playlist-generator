391c2cc367ef5e429db26f4ffd988fd3e20ac528
gnawre31 <erwangli2@gmail.com>
2022-08-15 21:18:48 -0400

    player styling

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -13,13 +13,25 @@ const Player = () => {
   if (tracks.length > 0) {
     return (
       <div>
-        <div>
+        <div className="z-0 fixed w-screen pr-8 pl-8 flex justify-center items-center" style={{ backgroundColor: "#1DB954", bottom: "0%", height: "15vh" }}>
           <SpotifyPlayer
             token={accessToken}
             autoPlay={tracks.length > 0 ? true : false}
             uris={tracks.map((t) => t.uri)}
             showSaveIcon={true}
             callback={(state) => updatePlayerState(spotifyDispatch, state)}
+            styles={{
+              activeColor: "#fff",
+              bgColor: "#1DB954",
+              color: "#fff",
+              loaderColor: "#fff",
+              sliderColor: "#fff",
+              trackArtistColor: "#fff",
+              trackNameColor: "#fff",
+              sliderTrackColor: "#179443",
+              sliderHandleColor: "#fff",
+              sliderTrackBorderRadius: "999px",
+            }}
           />
         </div>
       </div>
